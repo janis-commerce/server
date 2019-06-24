d11aabb8a8773d8621707b3baca4104ee7af2712
Juan <juan@fizzmod.com>
2019-06-24 11:16:19 -0300

    Fixing API Dispatcher constructors

--- debug.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-const Server = require('./lib');
-
-new Server({ cors: true });
---
+++ tests/server-test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// const { Server } = require('./..');
+
+/* eslint-disable prefer-arrow-callback */
+
+describe('Server', function() {
+
+
+});
